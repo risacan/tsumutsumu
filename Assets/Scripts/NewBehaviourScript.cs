7bfdd84de98bcf3d9d87fe79ca6dc8d8eb22cd72
Risa Harada <risa.harada@kidsstar.co.jp>
2017-04-25 19:02:27 +0900

    :thinking:

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -1,30 +1,103 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Security.AccessControl;
 using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class NewBehaviourScript : MonoBehaviour {
 
     public GameObject BallPrefab;
     public Sprite[] BallSprites;
 
+    private GameObject FirstBall;
+    private List<GameObject> RemovableBallList;
+    private GameObject LastBall;
+    private String CurrentBallName;
+
 	// Use this for initialization
 	void Start () {
-	    DropBalls(55);
+	    DropBalls(150);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+	    if (Input.GetMouseButton(0) && FirstBall == null) {
+	        OnDragStart();
+	    } else if (Input.GetMouseButtonUp(0)) {
+	        OnDragEnd();
+	    } else if (FirstBall != null) {
+	        OnDragging();
+	    }
 	}
 
-     public void DropBalls(int count) {
+     private void  DropBalls(int count) {
         for (var i = 0; i < count; i++) {
-            var ball = Instantiate(BallPrefab);
-            var randomX = Random.Range(-2.0f, 2.0f);
-            var randomZ = Random.Range(-40.0f, 40.0f);
-            int spriteId = Random.Range(0, 3);
+            var randomX = Random.Range(-0.5f, 0.5f);
+            float randomZ = Random.Range(-20.0f, 20.0f);
+            var ball = Instantiate(BallPrefab, new Vector3(randomX, 30.0f, 0.0f), Quaternion.identity);
+            ball.transform.eulerAngles = new Vector3(0, 0, randomZ);
+            int spriteId = Random.Range(0, 4);
             ball.name = "ball" + spriteId;
-            yield return new WaitForSeconds(0.05f);
+            var ballTexture = ball.GetComponent<SpriteRenderer>();
+            ballTexture.sprite = BallSprites[spriteId];
         }
+    }
+
+    private void OnDragStart() {
+        var currentCollider = GetCurrentCollider();
+        if (currentCollider != null) {
+            var currentColliderObject = currentCollider.gameObject;
+            if (currentColliderObject.name.IndexOf("ball") != -1) {
+                RemovableBallList = new List<GameObject>();
+                FirstBall = currentColliderObject;
+                CurrentBallName = currentColliderObject.name;
+            }
+        }
+    }
+
+    private void OnDragEnd() {
+        if (FirstBall != null) {
+            if (RemovableBallList.Count >= 3) {
+                foreach (GameObject obj in RemovableBallList) {
+                    Destroy(obj);
+                    DropBalls(1);
+                }
+            }
+        } else {
+            foreach (GameObject obj in RemovableBallList) {
+                var listedBall = obj;
+                listedBall.name = listedBall.name.Substring(0, 1);
+            }
+            FirstBall = null;
+        }
+    }
+
+    private void OnDragging() {
+        var currentCollider = GetCurrentCollider();
+        Debug.Log(currentCollider);
+        if (currentCollider != null) {
+            var currentColliderObject = currentCollider.gameObject;
+            if (currentColliderObject.name == CurrentBallName) {
+                if (LastBall != currentColliderObject) {
+                    var dist = Vector2.Distance(LastBall.transform.position, currentColliderObject.transform.position);
+                    if (dist <= 1.5) {
+                        PushToList(currentColliderObject);
+                    }
+                }
+            }
+        }
+    }
+
+    private Collider2D GetCurrentCollider() {
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        return hit.collider;
+    }
+
+    private void PushToList(GameObject obj) {
+        LastBall = obj;
+        RemovableBallList.Add(obj);
+        obj.name = "_" + obj.name;
     }
 }
