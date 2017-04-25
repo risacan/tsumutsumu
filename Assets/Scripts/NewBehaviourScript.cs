4f0673c7cad2c18bdde1582d0349b0bb7335faa1
Risa Harada <risa.harada@kidsstar.co.jp>
2017-04-25 15:10:00 +0900

    :seedling:

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public GameObject BallPrefab;
+    public Sprite[] BallSprites;
+
+	// Use this for initialization
+	void Start () {
+	    DropBalls(55);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+     public void DropBalls(int count) {
+        for (var i = 0; i < count; i++) {
+            var ball = Instantiate(BallPrefab);
+            var randomX = Random.Range(-2.0f, 2.0f);
+            var randomZ = Random.Range(-40.0f, 40.0f);
+            int spriteId = Random.Range(0, 3);
+            ball.name = "ball" + spriteId;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+}
